a80deee5858f4ec03d8e89ae96d03ce294d82ab6
Yevgen Grytsay <hrytsai@mti.ua>
2015-07-06 14:05:05 +0300

    Guzzle 5 not supported.

--- src/SleepingOwl/Apist/Apist.php
+++ src/SleepingOwl/Apist/Apist.php
@@ -5,232 +5,242 @@ use SleepingOwl\Apist\Methods\ApistMethod;
 use SleepingOwl\Apist\Selectors\ApistFilter;
 use SleepingOwl\Apist\Selectors\ApistSelector;
 use SleepingOwl\Apist\Yaml\YamlApist;
-use Symfony\Component\DomCrawler\Crawler;
 
 abstract class Apist
 {
-	/**
-	 * @var string
-	 */
-	protected $baseUrl;
-	/**
-	 * @var Client
-	 */
-	protected $guzzle;
-	/**
-	 * @var ApistMethod
-	 */
-	protected $currentMethod;
-	/**
-	 * @var ApistMethod
-	 */
-	protected $lastMethod;
-	/**
-	 * @var bool
-	 */
-	protected $suppressExceptions = true;
+    /**
+     * @var string
+     */
+    protected $baseUrl;
+    /**
+     * @var Client
+     */
+    protected $guzzle;
+    /**
+     * @var ApistMethod
+     */
+    protected $currentMethod;
+    /**
+     * @var ApistMethod
+     */
+    protected $lastMethod;
+    /**
+     * @var bool
+     */
+    protected $suppressExceptions = true;
 
-	/**
-	 * @param array $options
-	 */
-	function __construct($options = [])
-	{
-		$baseUrlKey = class_exists('GuzzleHttp\Url') ? 'base_url' : 'base_uri';
-		$options[$baseUrlKey] = $this->getBaseUrl();
-		$this->guzzle = new Client($options);
-	}
+    /**
+     * @param array $options
+     */
+    public function __construct(array $options = [])
+    {
+        $options['base_uri'] = $this->getBaseUrl();
+        $this->guzzle = new Client($options);
+    }
 
-	/**
-	 * @return Client
-	 */
-	public function getGuzzle()
-	{
-		return $this->guzzle;
-	}
+    /**
+     * @return Client
+     */
+    public function getGuzzle()
+    {
+        return $this->guzzle;
+    }
 
-	/**
-	 * @param Client $guzzle
-	 */
-	public function setGuzzle($guzzle)
-	{
-		$this->guzzle = $guzzle;
-	}
+    /**
+     * @param Client $guzzle
+     */
+    public function setGuzzle($guzzle)
+    {
+        $this->guzzle = $guzzle;
+    }
 
-	/**
-	 * Create filter object
-	 *
-	 * @param $cssSelector
-	 * @return ApistFilter
-	 */
-	public static function filter($cssSelector)
-	{
-		return new ApistSelector($cssSelector);
-	}
+    /**
+     * Create filter object
+     *
+     * @param $cssSelector
+     *
+     * @return ApistFilter
+     */
+    public static function filter($cssSelector)
+    {
+        return new ApistSelector($cssSelector);
+    }
 
-	/**
-	 * Get current node
-	 *
-	 * @return ApistFilter
-	 */
-	public static function current()
-	{
-		return static::filter('*');
-	}
+    /**
+     * Get current node
+     *
+     * @return ApistFilter
+     */
+    public static function current()
+    {
+        return static::filter('*');
+    }
 
-	/**
-	 * Initialize api from yaml configuration file
-	 *
-	 * @param $file
-	 * @return YamlApist
-	 */
-	public static function fromYaml($file)
-	{
-		return new YamlApist($file, []);
-	}
+    /**
+     * Initialize api from yaml configuration file
+     *
+     * @param $file
+     *
+     * @return YamlApist
+     */
+    public static function fromYaml($file)
+    {
+        return new YamlApist($file, []);
+    }
 
-	/**
-	 * @return ApistMethod
-	 */
-	public function getCurrentMethod()
-	{
-		return $this->currentMethod;
-	}
+    /**
+     * @return ApistMethod
+     */
+    public function getCurrentMethod()
+    {
+        return $this->currentMethod;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getBaseUrl()
-	{
-		return $this->baseUrl;
-	}
+    /**
+     * @return string
+     */
+    public function getBaseUrl()
+    {
+        return $this->baseUrl;
+    }
 
-	/**
-	 * @return ApistMethod
-	 */
-	public function getLastMethod()
-	{
-		return $this->lastMethod;
-	}
+    /**
+     * @return ApistMethod
+     */
+    public function getLastMethod()
+    {
+        return $this->lastMethod;
+    }
 
-	/**
-	 * @param string $baseUrl
-	 */
-	public function setBaseUrl($baseUrl)
-	{
-		$this->baseUrl = $baseUrl;
-	}
+    /**
+     * @param string $baseUrl
+     */
+    public function setBaseUrl($baseUrl)
+    {
+        $this->baseUrl = $baseUrl;
+    }
 
-	/**
-	 * @return boolean
-	 */
-	public function isSuppressExceptions()
-	{
-		return $this->suppressExceptions;
-	}
+    /**
+     * @return boolean
+     */
+    public function isSuppressExceptions()
+    {
+        return $this->suppressExceptions;
+    }
 
-	/**
-	 * @param boolean $suppressExceptions
-	 */
-	public function setSuppressExceptions($suppressExceptions)
-	{
-		$this->suppressExceptions = $suppressExceptions;
-	}
+    /**
+     * @param boolean $suppressExceptions
+     */
+    public function setSuppressExceptions($suppressExceptions)
+    {
+        $this->suppressExceptions = $suppressExceptions;
+    }
 
-	/**
-	 * @param $httpMethod
-	 * @param $url
-	 * @param $blueprint
-	 * @param array $options
-	 * @return array
-	 */
-	protected function request($httpMethod, $url, $blueprint, $options = [])
-	{
-		$this->currentMethod = new ApistMethod($this, $url, $blueprint);
-		$this->lastMethod = $this->currentMethod;
-		$this->currentMethod->setMethod($httpMethod);
-		$result = $this->currentMethod->get($options);
-		$this->currentMethod = null;
-		return $result;
-	}
+    /**
+     * @param $httpMethod
+     * @param $url
+     * @param $blueprint
+     * @param array $options
+     *
+     * @return array
+     */
+    protected function request($httpMethod, $url, $blueprint, $options = [])
+    {
+        $this->currentMethod = new ApistMethod($this, $url, $blueprint);
+        $this->lastMethod = $this->currentMethod;
+        $this->currentMethod->setMethod($httpMethod);
+        $result = $this->currentMethod->get($options);
+        $this->currentMethod = null;
 
-	/**
-	 * @param $content
-	 * @param $blueprint
-	 * @return array|string
-	 */
-	protected function parse($content, $blueprint)
-	{
-		$this->currentMethod = new ApistMethod($this, null, $blueprint);
-		$this->currentMethod->setContent($content);
-		$result = $this->currentMethod->parseBlueprint($blueprint);
-		$this->currentMethod = null;
-		return $result;
-	}
+        return $result;
+    }
 
-	/**
-	 * @param $url
-	 * @param $blueprint
-	 * @param array $options
-	 * @return array
-	 */
-	protected function get($url, $blueprint = null, $options = [])
-	{
-		return $this->request('GET', $url, $blueprint, $options);
-	}
+    /**
+     * @param $content
+     * @param $blueprint
+     *
+     * @return array|string
+     */
+    protected function parse($content, $blueprint)
+    {
+        $this->currentMethod = new ApistMethod($this, null, $blueprint);
+        $this->currentMethod->setContent($content);
+        $result = $this->currentMethod->parseBlueprint($blueprint);
+        $this->currentMethod = null;
 
-	/**
-	 * @param $url
-	 * @param $blueprint
-	 * @param array $options
-	 * @return array
-	 */
-	protected function head($url, $blueprint = null, $options = [])
-	{
-		return $this->request('HEAD', $url, $blueprint, $options);
-	}
+        return $result;
+    }
 
-	/**
-	 * @param $url
-	 * @param $blueprint
-	 * @param array $options
-	 * @return array
-	 */
-	protected function post($url, $blueprint = null, $options = [])
-	{
-		return $this->request('POST', $url, $blueprint, $options);
-	}
+    /**
+     * @param $url
+     * @param $blueprint
+     * @param array $options
+     *
+     * @return array
+     */
+    protected function get($url, $blueprint = null, $options = [])
+    {
+        return $this->request('GET', $url, $blueprint, $options);
+    }
 
-	/**
-	 * @param $url
-	 * @param $blueprint
-	 * @param array $options
-	 * @return array
-	 */
-	protected function put($url, $blueprint = null, $options = [])
-	{
-		return $this->request('PUT', $url, $blueprint, $options);
-	}
+    /**
+     * @param $url
+     * @param $blueprint
+     * @param array $options
+     *
+     * @return array
+     */
+    protected function head($url, $blueprint = null, $options = [])
+    {
+        return $this->request('HEAD', $url, $blueprint, $options);
+    }
 
-	/**
-	 * @param $url
-	 * @param $blueprint
-	 * @param array $options
-	 * @return array
-	 */
-	protected function patch($url, $blueprint = null, $options = [])
-	{
-		return $this->request('PATCH', $url, $blueprint, $options);
-	}
+    /**
+     * @param $url
+     * @param $blueprint
+     * @param array $options
+     *
+     * @return array
+     */
+    protected function post($url, $blueprint = null, $options = [])
+    {
+        return $this->request('POST', $url, $blueprint, $options);
+    }
 
-	/**
-	 * @param $url
-	 * @param $blueprint
-	 * @param array $options
-	 * @return array
-	 */
-	protected function delete($url, $blueprint = null, $options = [])
-	{
-		return $this->request('DELETE', $url, $blueprint, $options);
-	}
+    /**
+     * @param $url
+     * @param $blueprint
+     * @param array $options
+     *
+     * @return array
+     */
+    protected function put($url, $blueprint = null, $options = [])
+    {
+        return $this->request('PUT', $url, $blueprint, $options);
+    }
+
+    /**
+     * @param $url
+     * @param $blueprint
+     * @param array $options
+     *
+     * @return array
+     */
+    protected function patch($url, $blueprint = null, $options = [])
+    {
+        return $this->request('PATCH', $url, $blueprint, $options);
+    }
+
+    /**
+     * @param $url
+     * @param $blueprint
+     * @param array $options
+     *
+     * @return array
+     */
+    protected function delete($url, $blueprint = null, $options = [])
+    {
+        return $this->request('DELETE', $url, $blueprint, $options);
+    }
 
 }
